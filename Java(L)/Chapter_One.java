d515e0e8a89b39459cf741af9a37427c43c4aa59
SEUNGHYUN-PARK <shstyle812@gmail.com>
2018-04-21 23:05:45 +0900

    Java Review

---
+++ Java(L)/Chapter_One.java
@@ -0,0 +1,23 @@
+/*
+*  자바(Java Programming Language)
+*
+*  1996년 1월, Sun Microsystems, Inc. 에서 개발한 객체지향 프로그래밍 언어
+*  운영체제에 독립적 => 인터넷 환경에 적합안 언어
+*  객체지향 개념과 기존의 다른 프로그래밍 언어, 특히 C++의 장점ㅇ르 채택하는 동시에 잘 사용 되지 않는 부분은 과감히 제외
+*  제임스 고슬링 아서 밴 호프 공동 제작
+*  소규모 가전제품과 대규모 기업환경을 위한 소프트웨어 개발분야에 활용
+*  Gabage collector 가 자동적으로 메모리를 관리
+*  네트워크와 분산처리를 지원
+*  멀티쓰레드를 지원
+*  동적 로딩을 지원 => 실행 시, 모든 클래스가 로딩되지않고 필요한 시점에 클래스를 로딩하여 사용할 수 있는 장점
+*  속도가 느리다는 단점이 있으나, 성능개선을 통해 속도 개선
+*  .java -> .class -> exe -> 실행
+*
+ */
+public class Chapter_One
+{
+    public static void main(String[] args) // String args[]로도 작성 가능
+    {
+        System.out.println("Hello world.");
+    }
+}
