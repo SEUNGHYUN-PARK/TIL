c21bef1223f09d1825c4b240c1503243963dbd7b
PARKSEUNGHYUN <shstyle812@gmail.com>
2018-07-17 07:54:02 +0900

    Queue

--- Java/#17_Queue.java
+++ Java/#17_Queue.java
@@ -1,17 +1,13 @@
-import javafx.util.Pair;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
-
-class Pairs
+class Pair
 {
-    int x;
-    int y;
-    Pairs(int _x, int _y)
+    int x,y;
+    Pair(int _x,int _y)
     {
-        this.x = _x;
-        this.y = _y;
+        this.x=_x;
+        this.y=_y;
     }
 }
 
@@ -21,32 +17,54 @@ public class QueueMain {
         Queue
 
         선언
-        Queue<Wrapper> queue = new LinkedList<Integer>();
+        Queue<Object> queue = new LinkedList<>();
 
         메소드
-        add(object); // 객체 추가
-        remove(); // 객체 리턴 + 큐로부터의 삭제;
+        add(object) : 큐에 데이터 추가
+        peek(object) : 최전방에 있는 데이터 리턴
+        remove(object) : 큐의 최전방의 데이터 삭제
 
+        Pair같은 경우는 직접 만들어줘야한다... C++에만 있는 기능
+
+        Class Pair
+        {
+            int x,y;
+            Pair(int _x. int _y)
+            {
+                this.x=_x;
+                this.y=_y;
+            }
+        }
     */
 
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        Queue<Pairs> q = new LinkedList<Pairs>(); // Pair를 활용한
-        q.offer(new Pairs(10,20));
-        Pairs p = q.remove();
-        int x=p.x;
-        int y=p.y;
-        System.out.println(p.x+" "+p.y);
-
-        for (int i = 0; i < 5; i++) {
-            queue.offer(i);
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<5;i++)
+        {
+            queue.add(i);
         }
-        System.out.println(queue);
+        while(!queue.isEmpty())
+        {
+            int temp = queue.peek();
+            queue.remove();
+            System.out.println(temp);
+        }
 
-        queue.remove();
-        queue.offer(10);
-        System.out.println(queue.poll()); // 맨앞 데이터를 추출한다. 더불어 데이터도 삭제한다
-        System.out.println(queue.peek()); // 맨앞 데이터를 추출한다. 하지만 맨앞의 데이터는 삭제되지않는다.
+        Queue<Pair> queuepair = new LinkedList<Pair>();
+        queuepair.add(new Pair(10,20));
+        queuepair.add(new Pair(20,30));
+        queuepair.add(new Pair(30,40));
+        queuepair.add(new Pair(40,50));
+        queuepair.add(new Pair(50,60));
+        queuepair.add(new Pair(60,70));
+
+        while(!queuepair.isEmpty())
+        {
+            int nx = queuepair.peek().x;
+            int ny = queuepair.peek().y;
+            queuepair.remove();
+            System.out.println(nx+" "+ny);
+        }
 
     }
 }
